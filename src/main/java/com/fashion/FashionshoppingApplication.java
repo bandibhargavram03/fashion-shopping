c68d1c1dea1c9937ce16f0a04e8cd1585c241a68
Bhargav Ram Bandi <bandibhargav1999@gmail.com>
2023-11-11 12:27:31 +0530

    initial commit

---
+++ src/main/java/com/fashion/FashionshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.fashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionshoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fashion/FashionshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
